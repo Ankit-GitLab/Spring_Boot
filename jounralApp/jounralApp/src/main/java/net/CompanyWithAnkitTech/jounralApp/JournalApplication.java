6a5d8b342b4fe0bedabfdb9f99bc2d4da0b55a90
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-05-06 22:28:15 +0530

    journal app

---
+++ jounralApp/jounralApp/src/main/java/net/CompanyWithAnkitTech/jounralApp/JournalApplication.java
@@ -0,0 +1,14 @@
+package net.CompanyWithAnkitTech.jounralApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JounralAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JounralAppApplication.class, args);
+	}
+
+}
