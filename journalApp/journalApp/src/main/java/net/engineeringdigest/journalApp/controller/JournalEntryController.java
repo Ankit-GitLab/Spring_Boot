a79e98896eaa2cf1bc420e3415e7ebd45dbe2335
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-05-07 23:54:36 +0530

    journal App

---
+++ journalApp/journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,29 @@
+package net.engineeringdigest.journalApp.controller;
+
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){ //localhost:8080/journal Get
+        return new ArrayList<>(journalEntries.values());
+    }
+    @PostMapping
+    public void createEntry(){ //localhost:8080/journal Post
+
+    }
+}
