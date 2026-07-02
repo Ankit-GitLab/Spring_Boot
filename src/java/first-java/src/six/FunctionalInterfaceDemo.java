e5c9897130c6b10dc7ff2436dcde6f35895603b7
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-07-02 17:41:56 +0530

    lamda

--- src/java/first-java/src/six/FunctionalInterfaceDemo.java
+++ src/java/first-java/src/six/FunctionalInterfaceDemo.java
@@ -1,11 +1,17 @@
 package six;
 
 @FunctionalInterface
-interface BookAction{
+interface BookAction {
     void perform();
 }
+
+// parameter --> expression
+// () -> {}
 public class FunctionalInterfaceDemo {
     public static void main(String[] args) {
-
+        BookAction action =() ->  {
+                System.out.println("Action performed");
+        };
+        action.perform();
     }
 }
