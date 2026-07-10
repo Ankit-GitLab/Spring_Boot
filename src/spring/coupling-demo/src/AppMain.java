374ee90b6b18c0eb56a4b06d914df0543a8e768e
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-07-10 12:20:29 +0530

    spring

--- src/spring/coupling-demo/src/AppMain.java
+++ src/spring/coupling-demo/src/AppMain.java
@@ -1,8 +1,23 @@
+import loose.EmailNotificationService;
+import loose.NotificationService;
+import loose.SMSNotificationService;
 import tight.UserService;
 
 public class AppMain {
     public static void main(String[] args) {
+
+        //Tight coupling
         UserService userService = new UserService();
         userService.notifyUser("Order placed!");
+
+        //Loose coupling
+        NotificationService emailService = new EmailNotificationService();
+        loose.UserService userServiceLoose1 = new loose.UserService(emailService);
+
+        NotificationService smsService = new SMSNotificationService();
+        loose.UserService userServiceLoose = new loose.UserService(smsService);
+
+        userServiceLoose.notifyUser("Order Processed!");
+        userServiceLoose1.notifyUser("Order Processed!");
     }
 }
