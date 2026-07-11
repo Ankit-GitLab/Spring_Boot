6810f018b164a38a4503395d1d8e106a60294e5c
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-07-11 11:51:45 +0530

    java bean project

---
+++ spring/first-spring/src/main/java/com/example/demo/MainApp.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class MainApp {
+    public static void main(String[] args) {
+        GreetingService g1 = new GreetingService();
+        g1.sayHello();
+    }
+}
