7930c2074af622e3fbfb740ce04404e1c8e1047c
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-08-25 18:07:07 +0530

    updata

--- SpringExample/src/main/java/com/loose/coupling/WebServiceDataProvider.java
+++ SpringExample/src/main/java/com/loose/coupling/WebServiceDataProvider.java
@@ -1,4 +1,8 @@
 package com.loose.coupling;
 
 public class WebServiceDataProvider {
+    @Override
+    public String getUserDetails() {
+        return "Fetching Data From WebService";
+    }
 }
--- SpringExample/src/main/java/com/tight/coupling/UserDatabase.java
+++ SpringExample/src/main/java/com/tight/coupling/UserDatabase.java
@@ -1,4 +1,10 @@
 package com.tight.coupling;
+// A - MySQL, PostgreSQL
+// B - Web Service, MongoDB
 
 public class UserDatabase {
+    public String getUserDetails(){
+        // Directly access database here
+        return "User Details From Database";
+    }
 }
