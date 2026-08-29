27431aaec178568b8f42a6ab4f36ffcc053b0b45
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-08-29 21:48:50 +0530

    api test

---
+++ spring/first/src/main/java/com/demo/first/HelloController.java
@@ -0,0 +1,25 @@
+package com.demo.first;
+
+import com.demo.first.app.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World!";
+    }
+
+//    @GetMapping("/user")
+    @RequestMapping(value = "/user", method = {RequestMethod.GET, RequestMethod.POST})
+    public User getUser(){
+        User user = new User(1, "Moto","ankitrocky480@gmail.com");
+        return user;
+    }
+
+}
