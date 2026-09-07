9eea1fbd591dd8ebe5da2bf314d014f72799ae4c
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-09-07 14:47:34 +0530

    create database student table

--- database/jdbc-demo/src/main/java/JDBCDemo.java
+++ database/jdbc-demo/src/main/java/JDBCDemo.java
@@ -7,7 +7,18 @@ public class JDBCDemo {
     private static final String USER = "root";
     private static final String PASSWORD = "Ankit@123";
     public static void main(String[] args) {
-        Connection conn = null;
+
+        try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)){
+            System.out.println("Connected to the database!");
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+/*
+         Connection conn = null;
         try {
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Connected to the database!");
@@ -21,5 +32,4 @@ public class JDBCDemo {
                 throw new RuntimeException(e);
             }
         }
-    }
-}
+ */
